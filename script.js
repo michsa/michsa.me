01be0f544167b513ccdb3cad769035a9f680057c
git@michsa.me <git@michsa.me>
2019-02-07 13:46:51 -0500

    stagger blinkencolors

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-var bgcolors = ["#EFF", "#FEF", "#FFE", "#FEE", "#EFE", "#EEF"];
-var hicolors = ["#F0A", "#FA0", "#A0F", "#AF0", "#0FA", "#0AF"];
-function without(arr, x) {
-  var i = arr.indexOf(x);
-  if (i < 0) return arr;
-  return arr.slice(0, i).concat(arr.slice(i + 1, arr.length));
+// ["#E5FFFF", "#FFE5FF", "#FFFFE5", "#FFE5E5", "#E5FFE5", "#E5E5FF"]
+
+var colors = {
+  "bg": ["#EFF", "#FEF", "#FFE", "#FEE", "#EFE", "#EEF"],
+  "hi": ["#F0A", "#FA0", "#A0F", "#AF0", "#0FA", "#0AF"]
 }
 
-function updateColors(prevStyle, prevbg, prevhi) {
-  var bgoptions = prevbg ? without(bgcolors, prevbg) : bgcolors;
-  var hioptions = prevhi ? without(hicolors, prevhi) : hicolors;
+function without(arr, x) {
+  var i = arr.indexOf(x)
+  if (i < 0) return arr
+  return arr.slice(0, i).concat(arr.slice(i + 1, arr.length))
+}
 
-  var bgcolor = bgoptions[Math.floor(Math.random() * (bgoptions.length))];
-  var hicolor = hioptions[Math.floor(Math.random() * (hioptions.length))];
-  
-  if (prevStyle) {
-    console.log("found prev style!")
-    document.head.removeChild(prevStyle)
+function updateColors(prevStyle, prevColors, shouldUpdate) {
+  var newColors = {}
+  for (key in colors) {
+    var colorOptions = {}
+    if (shouldUpdate[key]) {
+      colorOptions[key] = key in prevColors ?
+        without(colors[key], prevColors[key]) : colors[key]
+    }
+    newColors[key] = shouldUpdate[key] ?
+      colorOptions[key][Math.floor(Math.random() * (colorOptions[key].length))]
+      : prevColors[key]
   }
-  var style = document.createElement("style");
-	style.appendChild(document.createTextNode(""));
-	document.head.appendChild(style);
-	style.sheet.insertRule("#header { background-color: " + bgcolor + 
-	    "; border-color: " + hicolor + " }", 0);
-	style.sheet.insertRule(".underline::after { border-color: " + hicolor + " }", 1);
-  style.sheet.insertRule("a { color: " + hicolor + " }", 2);
+
+  if (prevStyle) document.head.removeChild(prevStyle)
+  var style = document.createElement("style")
+  style.appendChild(document.createTextNode(""))
+  document.head.appendChild(style)
+
+  style.sheet.insertRule("#header { background-color: " + newColors.bg +
+    "; border-color: " + newColors.hi + " }", 0)
+
+  style.sheet.insertRule(".underline::after { border-color: " + newColors.hi + " }", 1)
+  style.sheet.insertRule("a { color: " + newColors.hi + " }", 2)
   // if hicolor matches our hover color (#A0F = purple), 
   // change the hover color #0AF = blue)
-  if (hicolor == "#A0F")
-    style.sheet.insertRule("a:hover { color: #0AF !important }", 3);
-    
-  setTimeout(function(){ updateColors(style, bgcolor, hicolor); }, 
-      Math.floor(Math.random() * 1200 + 2200));
-}
-window.onload = updateColors();
+  if (newColors.hi == "#A0F")
+    style.sheet.insertRule("a:hover { color: #0AF !important }", 3)
 
+  setTimeout(function () {
+    updateColors(style, newColors, { bg: !shouldUpdate.bg, hi: !shouldUpdate.hi })
+  }, Math.floor(Math.random() * 800 + 1000))
+}
+
+window.onload = updateColors(null, {}, { 'bg': false, 'hi': true })
